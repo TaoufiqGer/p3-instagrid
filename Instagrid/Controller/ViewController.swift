fc7bc558e48dcaa2daf61df9ef55c97d691b6c6e
Taoufiq Germoud <gt@oufiq.fr>
2021-07-29 13:49:44 +0200

    :sparkles: Add gridLayout feature

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -14,6 +14,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var topRow: UIStackView!
+    @IBOutlet weak var buttomRow: UIStackView!
+    
+    @IBAction func firstLayout(_ sender: UIButton) {
+        topRow.arrangedSubviews[0].isHidden = true
+        buttomRow.arrangedSubviews[0].isHidden = false
+        buttomRow.arrangedSubviews[1].isHidden = false
+    }
+    
+    @IBAction func secondLayout(_ sender: UIButton) {
+        topRow.arrangedSubviews[0].isHidden = false
+        topRow.arrangedSubviews[1].isHidden = false
+        buttomRow.arrangedSubviews[0].isHidden = true
+    }
+    @IBAction func thirdLayout(_ sender: UIButton) {
+        topRow.arrangedSubviews[0].isHidden = false
+        topRow.arrangedSubviews[1].isHidden = false
+        buttomRow.arrangedSubviews[0].isHidden = false
+        buttomRow.arrangedSubviews[1].isHidden = false
+    }
 }
 
