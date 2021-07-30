9d780b49d5403832fdb6facfe5b91573379f873c
Taoufiq Germoud <gt@oufiq.fr>
2021-07-30 18:49:17 +0200

    Refactor UI still buggy

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -13,26 +13,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBOutlet weak var topRow: UIStackView!
-    @IBOutlet weak var buttomRow: UIStackView!
+    
+    
+    @IBOutlet weak var layoutButtons: LayoutButtons!
     
     @IBAction func firstLayout(_ sender: UIButton) {
-        topRow.arrangedSubviews[0].isHidden = true
-        buttomRow.arrangedSubviews[0].isHidden = false
-        buttomRow.arrangedSubviews[1].isHidden = false
+        layoutButtons.style = .first
     }
     
     @IBAction func secondLayout(_ sender: UIButton) {
-        topRow.arrangedSubviews[0].isHidden = false
-        topRow.arrangedSubviews[1].isHidden = false
-        buttomRow.arrangedSubviews[0].isHidden = true
+        layoutButtons.style = .second
     }
+    
     @IBAction func thirdLayout(_ sender: UIButton) {
-        topRow.arrangedSubviews[0].isHidden = false
-        topRow.arrangedSubviews[1].isHidden = false
-        buttomRow.arrangedSubviews[0].isHidden = false
-        buttomRow.arrangedSubviews[1].isHidden = false
+        layoutButtons.style = .third
     }
 }
 
